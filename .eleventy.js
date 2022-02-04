b977f01be6c897e168ec64d7f5f418be52dc6508
Tej Chajed <tchajed@mit.edu>
2022-02-03 19:46:24 -0600

    Fix path prefix

--- .eleventy.js
+++ .eleventy.js
@@ -4,10 +4,10 @@ const sass = require("sass");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass, { sass });
   return {
+    pathPrefix: "/personal-website-demo",
     dir: {
       includes: "_templates",
       output: "_site",
-      pathPrefix: "/personal-website-demo",
     },
   };
 };
