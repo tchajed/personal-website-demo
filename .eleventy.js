85c5fa0704028fe0d1cab60eff8ca3a10824b746
Tej Chajed <tchajed@mit.edu>
2022-02-03 19:38:12 -0600

    Tweak some settings

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,13 @@
 const eleventySass = require("@grimlink/eleventy-plugin-sass");
 const sass = require("sass");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass, { sass });
   return {
     dir: {
       includes: "_templates",
       output: "_site",
-    }
+      pathPrefix: "/personal-website-demo",
+    },
   };
 };
