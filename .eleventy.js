2993d6c002e7753ce8d8ffb15e8f166cf7bb3ce4
Tej Chajed <tchajed@mit.edu>
2022-02-15 21:32:27 -0500

    Use auto-formatting from VSCode

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
 const eleventySass = require("@grimlink/eleventy-plugin-sass");
 const sass = require("sass");
 
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(eleventySass, { sass });
-  return {
-    pathPrefix: "/personal-website-demo",
-    dir: {
-      includes: "_templates",
-      output: "_site",
-    },
-  };
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(eleventySass, { sass });
+    return {
+        pathPrefix: "/personal-website-demo",
+        dir: {
+            includes: "_templates",
+            output: "_site",
+        },
+    };
 };
